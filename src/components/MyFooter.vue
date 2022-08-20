7aa8a509d858a3c419d66af5ec17d083b8241780
arefe <arefegh4@gmail.com>
2022-08-20 13:21:53 +0430

    first commit

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="text-center thefooter" style=" background-color: #D9D9D9;" >
+    <div class="container-fluid px-3 my-3">
+  <div class="row">
+    <div class="col">
+   <img src="">
+    <img src="">
+    </div>
+    <div class="col">
+            <div>
+          <h5>تماس با ما</h5>
+          <p>تلفن شرکت <span dir="ltr">۰۲۱-۱۱۱۱۴۵۷۸</span></p>
+          <p>آدرس: بابل - خیابان شریعتی - دانشگاه نوشیروانی</p>
+          <p>arefegh4@gmail.com</p>
+           <p>پشتیبانی ۲۴ ساعته</p>
+        </div>
+    </div>
+    <div class="col">
+    <img src="">
+    </div>
+  </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+};
+</script>
